e42986af59e9445ad917353c21dfb7c7c2760c48
Kyle <kyle@eastcode.ca>
2019-11-27 09:44:14 -0400

    dockerized app and added nodemon for rapid development

---
+++ docker/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+echo 'Running migrations'
+migrations.sh
+echo 'Running defaults' &&
+node bin/lamassu-apply-defaults &&
+echo 'Running registration' 
+node bin/lamassu-register admin &&
+echo 'Basic installations run'
+
+./node_modules/.bin/nodemon bin/lamassu-admin-server --dev &
+./node_modules/.bin/nodemon bin/lamassu-server --mockSms
